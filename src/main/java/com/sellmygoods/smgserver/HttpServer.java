8728febb6f0987086c68470916c1873e42b93c5e
sbhmam001 <sbhmam001@myuct.ac.za>
2020-09-21 14:33:46 +0200

    [UPDATE] Added port & rootDir logging

--- src/main/java/com/sellmygoods/smgserver/HttpServer.java
+++ src/main/java/com/sellmygoods/smgserver/HttpServer.java
@@ -1,13 +1,23 @@
 package com.sellmygoods.smgserver;
 
+import com.sellmygoods.smgserver.config.Configuration;
+import com.sellmygoods.smgserver.config.ConfigurationManager;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+/**
+ * Entry class for http server [Driver]
+ */
 public class HttpServer {
-
     private static final Logger LOG = LoggerFactory.getLogger(HttpServer.class);
 
     public static void main(String[] args) {
-        LOG.info("Starting server...");
+        LOG.info("Server starting...");
+
+        ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http-config.json");
+        Configuration config = ConfigurationManager.getInstance().getCurrentlyLoadedConfiguration();
+
+        LOG.info("Using port : " + config.getPort());
+        LOG.info("Using rootDir : " + config.getRootDir());
     }
 }
