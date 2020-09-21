3512c552f7392374cdf063f9fa79e0dc5a6e3b1c
sbhmam001 <sbhmam001@myuct.ac.za>
2020-09-21 15:56:41 +0200

    [UPDATE] Instantiate socket accepter thread

--- src/main/java/com/sellmygoods/smgserver/HttpServer.java
+++ src/main/java/com/sellmygoods/smgserver/HttpServer.java
@@ -2,8 +2,11 @@ package com.sellmygoods.smgserver;
 
 import com.sellmygoods.smgserver.config.Configuration;
 import com.sellmygoods.smgserver.config.ConfigurationManager;
+import com.sellmygoods.smgserver.core.SocketAccepterThread;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
 
 /**
  * Entry class for http server [Driver]
@@ -17,7 +20,15 @@ public class HttpServer {
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http-config.json");
         Configuration config = ConfigurationManager.getInstance().getCurrentlyLoadedConfiguration();
 
-        LOG.info("Using port : " + config.getPort());
-        LOG.info("Using rootDir : " + config.getRootDir());
+        LOG.info("Using port : {}", config.getPort());
+        LOG.info("Using rootDir : {}", config.getRootDir());
+
+        try {
+            SocketAccepterThread socketAccepterThread = new SocketAccepterThread(config.getPort(), config.getRootDir());
+            socketAccepterThread.start();
+        } catch (IOException e) {
+            LOG.error("Trouble creating server socket");
+        }
+
     }
 }
